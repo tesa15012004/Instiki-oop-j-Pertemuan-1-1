73453f31f1dd5994b845fb09772baf99782a1d47
tesa15012004 <tesadahat@gmail.com>
2025-03-04 21:39:02 +0800

    Pertemuan 1

---
+++ Pertemuan01/src/pertemuan01/Pertemuan01.java
@@ -0,0 +1,18 @@
+
+package pertemuan01;
+
+
+/**
+ *
+ * @author Thesa Dahat
+ */
+public class Pertemuan01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.print ("Belajar Bahasa Java");
+    }
+    
+}
